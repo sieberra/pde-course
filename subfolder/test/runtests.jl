60c98a80b5f4cf13af44fd8df801dfa2e295b592
sieberra <raphaelsieber@icloud.com>
2021-11-11 11:22:56 +0100

    comments

--- subfolder/test/runtests.jl
+++ subfolder/test/runtests.jl
@@ -13,21 +13,21 @@ end
 ## Reference tests for H, qx
 
 # fix the random number generator, so the indices are the same in every test.
-Random.seed!(11)
-index_H = Int64.(round.(rand(20).*(size(H).-1))).+1  # -1, +1 makes sure the index is not 0, although the chance is small
+Random.seed!(11);
+index_H = Int64.(round.(rand(20).*(size(H).-1))).+1;  # -1, +1 makes sure the index is not 0, although the chance is small
 
 # Repeat for qx
-Random.seed!(12)
-index_qx = Int64.(round.(rand(20).*(size(qx).-1))).+1  
+Random.seed!(12);
+index_qx = Int64.(round.(rand(20).*(size(qx).-1))).+1;
 
 # I find the test arranged in testsets to be more pleasing to look at in the output.
-@testset "random-indices-H-qx" begin
-    @test_reference "ref-tests/H2.test" H[index_H]       # save the values found for the indices in a test file
-    @test_reference "ref-tests/qx2.test" qx[index_qx]
-end
+#@testset "random-indices-H-qx" begin
+#    @test_reference "ref-tests/H2.test" H[index_H]       # save the values found for the indices in a test file
+#    @test_reference "ref-tests/qx2.test" qx[index_qx]
+#end
 
 # repeat the reference test for arbitrary but not random indices
-@testset "arbitrary-indices" begin
-    @test_reference "ref-tests/H.test" H[[1,2,3,5,8,13,21,34,55,89,11,22,33,44,66,77,88,99,111,122]]
-    @test_reference "ref-tests/qx.test" qx[[1,2,3,5,8,13,21,34,55,89,11,22,33,44,66,77,88,99,111,122]]
-end
+#@testset "arbitrary-indices" begin
+#    @test_reference "ref-tests/H.test" H[[1,2,3,5,8,13,21,34,55,89,11,22,33,44,66,77,88,99,111,122]]
+#    @test_reference "ref-tests/qx.test" qx[[1,2,3,5,8,13,21,34,55,89,11,22,33,44,66,77,88,99,111,122]]
+#end
